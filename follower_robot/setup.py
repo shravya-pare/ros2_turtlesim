fbda480baa3a632323a94f6ed881f9c6df4d6622
shravya-pare <149505513+shravya-pare@users.noreply.github.com>
2023-11-24 07:55:04 +0530

    Add files via upload

---
+++ follower_robot/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+
+package_name = 'follower_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('launch/*'))
+        
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shravya',
+    maintainer_email='shravya@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'capture = follower_robot.capture_image:main',
+            'line = follower_robot.line_follow:main',
+        ],
+    },
+)
